2c9e68b4c13acce1ac3a425ebf8da883268f744b
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-04 19:12:31 +0000

    separted function calling in index.js,contains only function logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,26 @@
 const request = require('request');
 
 
-const args = (process.argv.length > 2) ? process.argv.slice(2) : '';
-// if no breed provided
-if (args === '') {
-  console.log('No search query provided');
-  return;
-}
-
-const BREED = args[0]; // saving the breed provided in the arguments
-
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${BREED}`; // url query to search for the breed
-
-
-request(URL, (error, response, body) => {
-
-  if (error) {
-    console.log(`Erorr: ${error}`);
-  } else {
-    let data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Error: requested breed is not found');
+const fetchBreedDescription = function(breedName, callback) {
+  
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`; // url query to search for the breed
+  
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(`\n Breed Description: \n${data[0].description}\n`);
+      let data = JSON.parse(body);
+      if (data.length === 0) {
+        error = 'No Description found';
+        callback(error, []);
+      } else {
+        callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+
+module.exports = {
+  fetchBreedDescription
+};
